effb2873a2418af681ba86ff284e294f16b8e866
minsu <minsu2530@u.sogang.ac.kr>
2022-07-27 23:43:06 +0900

    [FINISH]implement lexer

--- token/token.go
+++ token/token.go
@@ -14,10 +14,20 @@ const(
 	//식별자 + 리터럴
 	IDENT		= "IDENT"
 	INT			= "INT"
-
+	
 	//연산자
 	ASSIGN		= "="
 	PLUS		= "+"
+	MINUS		= "-"
+	BANG		= "!"
+	ASTERISK	= "*"
+	SLASH		= "/"
+
+	LT			= "<"
+	GT			= ">"
+
+	EQ			= "=="
+	NOT_EQ		= "!="
 
 	//구분자
 	COMMA		= ","
@@ -30,11 +40,22 @@ const(
 	//예약어
 	FUNCTION	= "FUNCTION"
 	LET			= "LET"
+	TRUE		= "TRUE"
+	FALSE		= "FALSE"
+	IF			= "IF"
+	ELSE		= "ELSE"
+	RETURN		= "RETURN"
+
 )
 
 var keywords = map[string]TokenType{
 	"fn": FUNCTION,
 	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
